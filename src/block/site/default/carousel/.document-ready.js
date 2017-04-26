862048f7d1095820049d0fa83143347ec5a6cfd0
azbn <i@azbn.ru>
2017-04-26 09:19:09 +0300

    201704260919

--- src/block/site/default/carousel/.document-ready.js
+++ src/block/site/default/carousel/.document-ready.js
@@ -1,5 +1,15 @@
-$("#objacts-carousel").carousel(); 
-$("#about-carousel").carousel({
-  	//swipe: 10 
-});
+$("#objacts-carousel").carousel();
 $("#blog-carousel").carousel();
+if(screenJS.isXS() || screenJS.isSM()) {
+	
+	$("#about-carousel").carousel({
+		interval : 0,
+	});
+	
+} else {
+	
+	$("#about-carousel").carousel({
+		//swipe: 10 
+	});
+	
+}
