7c9ccf6db86c2b3cae3539aeb72899a9eda0b315
caezar47 <iss47@ya.ru>
2017-08-03 11:50:10 +0300

    fix owl

--- src/js/owl.jquery/owl.ready.js
+++ src/js/owl.jquery/owl.ready.js
@@ -18,4 +18,44 @@ $(function() {
 			}
 		}
 	});	
+	$('.buyers-panel__owl').owlCarousel({
+		navText: ['<svg class="icon-svg icon-arrow-left-900" role="img"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/wp-content/themes/azbnbasetheme/img/svg/sprite.svg#arrow-left-900"></use></svg>', '<svg class="icon-svg icon-arrow-right-900" role="img"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/wp-content/themes/azbnbasetheme/img/svg/sprite.svg#arrow-right-900"></use></svg>'],
+		nav: true,
+		dots: true,
+		smartSpeed: 500,
+		//loop: true,
+		//mouseDrag: false,
+		responsive : {
+			0 : {
+				items:1,
+				margin: 0,
+			},
+			570 : {
+				items:2,
+				margin: 30,
+			}
+		}
+	});	
+	$('.objacts-panel__owl').owlCarousel({
+		navText: ['<svg class="icon-svg icon-arrow-left-900" role="img"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/wp-content/themes/azbnbasetheme/img/svg/sprite.svg#arrow-left-900"></use></svg>', '<svg class="icon-svg icon-arrow-right-900" role="img"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/wp-content/themes/azbnbasetheme/img/svg/sprite.svg#arrow-right-900"></use></svg>'],
+		nav: true,
+		dots: true,
+		smartSpeed: 500,
+		//loop: true,
+		//mouseDrag: false,
+		responsive : {
+			0 : {
+				items:1,
+				margin: 0,
+			},
+			570 : {
+				items:2,
+				margin: 30,
+			},
+			1025 : {
+				items:3,
+				margin: 30,
+			}
+		}
+	});	
 });
